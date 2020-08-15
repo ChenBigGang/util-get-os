bd2882ecf8b3abc95064449af6e803b387870ad4
陈刚 <1060652401@qq.com>
2020-08-15 17:00:29 +0800

    [init]初始化

---
+++ src/main/java/site/twofish/util/os/OSUtils.java
@@ -0,0 +1,13 @@
+package site.twofish.util.os;
+
+/**
+ * 获取操作系统信息
+ *
+ * @author ganggang
+ * @since 2020/8/15
+ */
+public class OSUtils {
+    public static String getOs() {
+        return "windows";
+    }
+}
